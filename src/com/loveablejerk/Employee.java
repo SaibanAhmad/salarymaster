78793ae79da4eceaef7a4465cfa060fdc83ba6e5
saibanahmadcs@gmail.com <saibanahmadcs@gmail.com>
2019-09-24 01:28:48 +0500

    Program Completed

--- src/com/loveablejerk/Employee.java
+++ src/com/loveablejerk/Employee.java
@@ -2,8 +2,30 @@ package com.loveablejerk;
 
 public class Employee {
 
-    public employeeSalary(double basicSalary, double target){
+    double totalSalary,commission =500;
+    int bonus = 5000, target = 20;
 
-    }
+   public Employee(String empName, double basicSalary, double sales){
 
+       isTargetAchieved(empName,basicSalary,sales);
+
+   }
+
+
+   public void isTargetAchieved(String empName,double basicSalary, double sales){
+       if(sales <= target){
+           commission = sales * 500;
+           totalSalary = basicSalary + commission;
+           System.out.println("Hello! " + empName + " Your Total salary for this month with commission of Rs 500 per sale is Rs: " + totalSalary);
+       }
+
+       else{
+           commission = commission * sales * 2;
+           totalSalary = bonus + basicSalary + commission;
+           System.out.println("Hello! " + empName + " Congratulations!!! you are qualified for the Bonus and Double Commission this month");
+           System.out.println("Special Bonus of Rs 5000 and Commission of Rs 1000 per sale Has been added in your salary");
+           System.out.println("Now Your Total Salary For this Month is Rs: " + totalSalary);
+       }
+   }
 }
+
--- src/com/loveablejerk/Main.java
+++ src/com/loveablejerk/Main.java
@@ -1,8 +1,22 @@
 package com.loveablejerk;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Enter Employee's Name: ");
+        String employeeName = new Scanner(System.in).next();
+
+        System.out.println("What is your Basic Monthly Salary: ");
+        int basicSalary = new Scanner(System.in).nextInt();
+
+        System.out.println("how many sales did you made in this month: ");
+        int salesMade = new Scanner(System.in).nextInt();
+
+
+        Employee myEmployee = new Employee(employeeName,basicSalary,salesMade);
 
     }
 }
